1d62517c307ed82e2607bd2d4bdbb232d842ea6e
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-01-25 14:43:57 +0530

    text styles

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,22 @@ void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.lightBlue[900],
-          title: Text('Hey Flutter'),
+          title: Text('Flutter Header'),
           centerTitle: true,
         ),
-        body: Center(child: Text("This is body")),
+        body: Center(
+            child: Text(
+          "This is body",
+          style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Color.fromARGB(255, 49, 49, 49),),
+        )),
         floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.lightBlue[900],
-          child: Text("Click!"),
-          onPressed: ()=>{}
-        ),
+            backgroundColor: Colors.lightBlue[900],
+            child: Text("Click!"),
+            onPressed: () => {}),
         backgroundColor: Colors.lightBlue[50],
       ),
     ));
