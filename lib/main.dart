2cac7d02e1ecf2e564f1833a093a5c66ef981b90
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-01-26 13:10:44 +0530

    Add local image

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class Test extends StatelessWidget{
           centerTitle: true,
         ),
         body: Center(
-            child: Image(image: NetworkImage("https://images.unsplash.com/photo-1536859975388-b5e6623e9223?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"),)
+            child: Image.asset("assets/wallpaper.jpg")
         
         ),
         floatingActionButton: FloatingActionButton(
