ef0954a9b6793f670ea54bdbc91a9dcd5632cbd5
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-01-26 13:41:08 +0530

    Added icon to button

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,37 @@
 // ignore_for_file: prefer_const_constructors
+// ignore_for_file: avoid_print
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Test(),
     ));
 
-class Test extends StatelessWidget{
+class Test extends StatelessWidget {
   const Test({Key? key}) : super(key: key);
   // Test({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        title: Text('Flutter Header'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: ElevatedButton.icon(
+            onPressed: ()=>{},
+            icon: Icon(
+              Icons.mail
+            ),
+            label: Text("Mail me"),
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+          )
+          ),
+      floatingActionButton: FloatingActionButton(
           backgroundColor: Color.fromARGB(255, 0, 0, 0),
-          title: Text('Flutter Header'),
-          centerTitle: true,
-        ),
-        body: Center(
-            child: Image.asset("assets/wallpaper.jpg")
-        
-        ),
-        floatingActionButton: FloatingActionButton(
-            backgroundColor: Color.fromARGB(255, 0, 0, 0),
-            child: Text("Click!"),
-            onPressed: () => {}),
-        backgroundColor: Color.fromARGB(255, 235, 238, 240),
-      );
+          child: Text("Click!"),
+          onPressed: () => {}),
+      backgroundColor: Color.fromARGB(255, 235, 238, 240),
+    );
   }
 }
