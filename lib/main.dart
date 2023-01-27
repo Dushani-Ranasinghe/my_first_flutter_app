45d6f277cd038a215af6e23f819892cf280b7fec
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-01-27 15:15:24 +0530

    changed configuration file, added elevated button

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,36 @@
-// ignore_for_file: prefer_const_constructors
-// ignore_for_file: avoid_print
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Test(),
-    ));
+void main() => runApp(MyApp());
 
-class Test extends StatelessWidget {
-  const Test({Key? key}) : super(key: key);
-  // Test({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 0, 0, 0),
-        title: Text('Flutter Header'),
-        centerTitle: true,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        margin: EdgeInsets.all(10.0),
-        color: Colors.grey[400],
-        child: Text("Hello"),
-      ),
-      floatingActionButton: FloatingActionButton(
-          backgroundColor: Color.fromARGB(255, 0, 0, 0),
-          child: Text("Click!"),
-          onPressed: () => {}),
-      backgroundColor: Color.fromARGB(255, 235, 238, 240),
-    );
+    return MaterialApp(
+        // remove debug banner
+        debugShowMaterialGrid: false,
+        //this is home widget
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.lightBlue[900],
+            title: Text("Header"),
+            centerTitle: true,
+          ),
+          body: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => {},
+                  child: Text("click"),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
