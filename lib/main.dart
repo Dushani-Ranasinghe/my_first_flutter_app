b2c70cc4d9a4ee6a5aa79195dcb9f8e910aed0a4
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-14 19:47:37 +0530

    GridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_flutter_app/screens/insta_layout.dart';
+// import 'package:my_first_flutter_app/screens/insta_layout.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,22 +18,48 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  
-  final List _posts = ['post 1', 'post 2', 'post 3', 'post 4'];
-  final List _stories = ['story1', 'story 2', 'story 3', 'story 4', 'story 5'];
+  // final List _posts = ['post 1', 'post 2', 'post 3', 'post 4'];
+  // final List _stories = ['story1', 'story 2', 'story 3', 'story 4', 'story 5'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         // appBar: AppBar(title: Text("Header")),
-        body: Center(child: ClipRRect(
-          borderRadius: BorderRadius.circular(40),
-          child: Container(
-                height: 300,
-                width: 300,
-                decoration: BoxDecoration( color: Colors.amber[400]),
-              ),
-        ),
-            // InstaLayout(stories: _stories, posts: _posts),
-        ));
+        body: GridView.count(crossAxisCount: 1, children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue[100],
+            ),
+          ),
+            Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue[100],
+            ),
+          ),
+            Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue[100],
+            ),
+          ),
+            Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue[100],
+            ),
+          ),
+          
+        ],));
   }
 }
+
+ // InstaLayout(stories: _stories, posts: _posts),
