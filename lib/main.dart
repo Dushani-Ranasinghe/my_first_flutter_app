0378b075016544cb3d0f0e247ee51ca638dfb5b0
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-15 09:58:16 +0530

    Created bottom nav

--- lib/main.dart
+++ lib/main.dart
@@ -28,41 +28,61 @@ class _HomePageState extends State<HomePage> {
   //variable
   int number = 0;
   //method
-  void increaseNum(){
+  void increaseNum() {
     setState(() {
       number++;
+    });
+  }
+
+  // bottom navigation bar
+  int _selectedNavIndex = 0;
+
+  void _setBottomNav(int index){
+    setState(() {
+      _selectedNavIndex = index ;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(title: Text("Header")),
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(
-            "Tapped $number times",
-            style: TextStyle(fontSize: 20),
-          ),
-          GestureDetector(
-            onTap: increaseNum,
-            child: Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.blue,
-              ),
-              child: Text(
-                "Tap Here",
-                style: TextStyle(fontSize: 20),
+      // appBar: AppBar(title: Text("Header")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "Tapped $number times",
+              style: TextStyle(fontSize: 20),
+            ),
+            GestureDetector(
+              onTap: increaseNum,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  "Tap Here",
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ));
+      bottomNavigationBar:
+          BottomNavigationBar(
+            currentIndex: _selectedNavIndex,
+            onTap: _setBottomNav,
+            type: BottomNavigationBarType.fixed, items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+        BottomNavigationBarItem(icon: Icon(Icons.message), label: "message"),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: "account"),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: "settings")
+      ]),
+    );
   }
 }
 
