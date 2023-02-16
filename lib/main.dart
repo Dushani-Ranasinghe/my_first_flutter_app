b86fe6a95f66c5c609d39d6d5dbaad3aaf15bb86
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-16 15:45:03 +0530

    Alert dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 // import 'user_pages/tab_bar.dart';
@@ -16,25 +17,60 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Demo Application",
       theme: ThemeData(
-          primarySwatch: Colors.blue,
-          textTheme: TextTheme(bodyText2: TextStyle(fontSize: 35))),
+          primarySwatch: Colors.blue,),
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void _showDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.pink[100],
+            title: Text("Alert!"),
+            content: Text("This is an alert."),
+            actions: [
+              MaterialButton(
+                onPressed: (() {}),
+                child: Text("OK"),
+              ),
+               MaterialButton(
+                onPressed: (() {
+                  Navigator.pop(context);
+                }),
+                child: Text("Cancel"),
+              )
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.pink[50],
       body: Center(
-        child: Column(children: [
-          Text('Height: ${MediaQuery.of(context).size.height.toString()}'),
-          Text('Width: ${MediaQuery.of(context).size.width.toString()}'),
-          Text('Aspect ratio: ${MediaQuery.of(context).size.aspectRatio.toStringAsFixed(2)}'),
-        ]),
+        child: MaterialButton(
+          color: Colors.deepPurple[100],
+          onPressed: _showDialog,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Click Here",
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ),
       ),
     );
   }
