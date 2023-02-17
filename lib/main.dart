185f627ff2efc050e227d357a55b8b6506e69f3f
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-17 10:35:07 +0530

    Stack widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 // import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'user_pages/user_page_view.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -29,8 +27,26 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       // backgroundColor: Colors.pink[100],
       body: Center(
-        child: UserPageView(),
+        child: UserStack(),
       ),
+    );
+  }
+}
+
+class UserStack extends StatelessWidget {
+  const UserStack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: [
+        //bottom
+        Container(height: 300, width: 300, color: Colors.blue[100],),
+        Container(height: 200, width: 200, color: Colors.pink[100],),
+        //top
+        Container(height: 100, width: 100, color: Colors.deepPurple[100],)
+      ],
     );
   }
 }
